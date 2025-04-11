1e1818820b390dab0613458e7a0f875061c8e825
EvgeniyaYakunina <Yakoonina@gmail.com>
2025-04-11 17:15:31 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Updated project!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count}$
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
