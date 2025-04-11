2b8409efa75f241e090a5a6d93fd5a7ed03897f6
EvgeniyaYakunina <Yakoonina@gmail.com>
2025-04-11 13:17:14 +0300

    fix imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+// https://vite.dev/config/
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/ui-kit-tailwind/",
   build: {
