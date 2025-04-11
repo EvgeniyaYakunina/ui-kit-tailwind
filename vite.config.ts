02dd30a02c5e05038408bb602e5d9eb225473701
EvgeniyaYakunina <Yakoonina@gmail.com>
2025-04-11 13:00:53 +0300

    fix branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react-swc"
 // https://vite.dev/config/
 export default defineConfig({
   base: "/ui-kit-tailwind/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   plugins: [react()],
 })
